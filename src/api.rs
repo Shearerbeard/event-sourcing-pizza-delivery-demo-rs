9e26ad4f5814a3fcfe1ba62077cc095c16a214bf
Mike Shearer <shearerbeard@gmail.com>
2022-03-03 14:46:27 -0700

    Move command and read functionality to service out of web

--- src/api.rs
+++ src/api.rs
@@ -1,14 +1,36 @@
-use eventstore::Client;
-use thalo_eventstoredb::ESDBEventStore;
 
-use crate::order::projection::OrderProjection;
+use eventstore::Client;
+use serde::{Serialize, Deserialize};
+use thalo::event_store::EventStore;
+use thalo_eventstoredb::ESDBEventStore;
+use uuid::Uuid;
+
+use crate::order::{
+    aggregate::{Address, LineItem, Order, OrderCommand, OrderPlacedEvent, self},
+    projection::{OrderProjection, OrderView},
+};
 
 pub struct OrderService {
-    pub event_store: ESDBEventStore,
+    event_store: ESDBEventStore,
     pub orders_projection: OrderProjection,
 }
 
+#[derive(Debug)]
+pub enum Error {
+    PlaceOrderError(aggregate::Error),
+    EventStoreError(thalo_eventstoredb::Error),
+}
+
+
+#[derive(Serialize , Deserialize, Debug, Clone)]
+pub struct PlaceOrderArgs {
+    order_type: String,
+    line_items: Vec<LineItem>,
+    address: Option<Address>,
+}
+
 impl OrderService {
+
     pub fn new(client: Client) -> Self {
         println!("Calling Order Service Init!");
 
@@ -16,5 +38,26 @@ impl OrderService {
             event_store: ESDBEventStore::new(client),
             orders_projection: OrderProjection::default(),
         }
+    }
+
+    pub async fn command_place_order(
+        &self,
+        PlaceOrderArgs {
+            order_type,
+            line_items,
+            address,
+        }: PlaceOrderArgs,
+    ) -> Result<OrderPlacedEvent, Error> {
+        self.event_store
+            .execute(Uuid::new_v4().to_string(), |order: &Order| {
+                order.order_placed(order_type.clone(), line_items.clone(), address.clone())
+            })
+            .await
+            .map_err(Error::EventStoreError)?
+            .map_err(Error::PlaceOrderError)
+    }
+
+    pub async fn read_all_orders(&self) -> Result<Vec<OrderView>, Error> {
+        Ok(self.orders_projection.get_all())
     }
 }
--- src/web.rs
+++ src/web.rs
@@ -1,45 +1,27 @@
 use actix_web::{get, post, web, HttpResponse, Responder};
-use serde::{Deserialize, Serialize};
-use thalo::event_store::EventStore;
-use uuid::Uuid;
 
-use crate::{order::{
-    aggregate::{LineItem, OrderCommand, Order, Address},
-}, api::OrderService};
+use crate::api::{PlaceOrderArgs, OrderService};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct PlaceOrderRequest {
-    order_type: String,
-    line_items: Vec<LineItem>,
-    address: Option<Address>
-}
 
 #[post("/order")]
 async fn place_order(
-    order_request: web::Json<PlaceOrderRequest>,
+    order_request: web::Json<PlaceOrderArgs>,
     service: web::Data<OrderService>,
 ) -> impl Responder {
-    let event = service.event_store
-        .execute(Uuid::new_v4().to_string(), |order: &Order| {
-            order.order_placed(
-                order_request.order_type.clone(),
-                order_request.line_items.clone(),
-                order_request.address.clone(),
-            )
-        })
-        .await;
+    let data = order_request.into_inner();
+    let event = service.command_place_order(data.clone()).await;
 
     match event {
         Ok(event) => println!("GOT EVENT! {:?}", event),
         Err(e) => println!("GOT ERROR {:?}", e),
     }
 
-    HttpResponse::Ok().body(format!("Order: {order:?}", order = order_request))
+    HttpResponse::Ok().body(format!("Order: {order:?}", order = data))
 }
 
 #[get("/order")]
 async fn get_orders(service: web::Data<OrderService>) -> impl Responder {
-    let all = service.orders_projection.get_all();
+    let all = service.read_all_orders().await.unwrap();
     println!("GOT All! {:?}", all);
     HttpResponse::Ok().body("All orders!")
 }
