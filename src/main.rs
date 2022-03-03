36d4617696b8d789657bb9f1ae39ed4dbbd90651
Mike Shearer <shearerbeard@gmail.com>
2022-03-02 21:01:38 -0700

    Fixes web server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, web::Data};
 use eventstore::{Client, ClientSettings};
 use order::projection::OrderProjection;
 use thalo_eventstoredb::ESDBEventStore;
@@ -7,65 +7,21 @@ use web::WebServer;
 mod order;
 mod web;
 
-// enum Error {
-//     ClientSettingsError(ClientSettingsParseError),
-// }
-
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//    let settings = "esdb://localhost:2113?tls=false"
-//        .parse::<ClientSettings>()
-//        // .map_err(Error::ClientSettingsParseError)
-//        .unwrap();
-//    let client = Client::new(settings).unwrap();
-//    let event_store = ESDBEventStore::new(client);
-//     let order_id = String::from("1");
-
-//     let exists = event_store
-//         .load_aggregate_sequence::<Order>(&order_id)
-//         .await
-//         .unwrap()
-//         .is_some();
-
-//     if exists {
-//         println!("EXISTS");
-//     } else {
-//         println!("DOES NOT EXIST... CREATING");
-//         let event = event_store
-//             .execute(order_id, |s: &Order| {
-//                 s.order_placed(
-//                     "Carryout".to_string(),
-//                     vec![aggregate::LineItem {
-//                         item_id: "1".to_string(),
-//                         quantity: 1,
-//                         notes: None,
-//                     }],
-//                     None,
-//                 )
-//             })
-//             .await;
-
-//         match event {
-//             Ok(event) => println!("GOT EVENT! {:?}", event),
-//             Err(e) => println!("GOT ERROR {:?}", e),
-//         }
-//     }
-//     Ok(())
-// }
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         let settings = "esdb://localhost:2113?tls=false"
             .parse::<ClientSettings>()
-            // .map_err(Error::ClientSettingsParseError)
             .unwrap();
 
+        let event_store = ESDBEventStore::new(Client::new(settings).unwrap());
+        let orders_projection = OrderProjection::default();
+
         App::new()
-            .app_data(WebServer {
-                event_store: ESDBEventStore::new(Client::new(settings.clone()).unwrap()),
-                orders_projection: OrderProjection::default(),
-            })
+            .app_data(Data::new(WebServer {
+                event_store,
+                orders_projection,
+            }))
             .service(web::place_order)
             .service(web::get_orders)
     })
